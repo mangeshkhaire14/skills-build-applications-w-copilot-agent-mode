e32bce58dab822b53a9db0cc71529c69f6f4e251
mangeshkhaire14 <mangeshkhaire14@gmail.com>
2026-04-05 09:46:36 +0000

    Update API URLs to use CODESPACE_NAME and restrict ALLOWED_HOSTS for Codespace and localhost

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,18 @@ SECRET_KEY = 'django-insecure-2_841fj6uq8am0s4_xc@n6xdo!w=fghw1=r$06p7p@wkdr(pg(
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+# Allow Codespace URL and localhost
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    ALLOWED_HOSTS = [
+        f"{codespace_name}-8000.app.github.dev",
+        'localhost',
+        '127.0.0.1',
+    ]
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
